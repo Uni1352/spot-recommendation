bcd6eac3d9808ae3c4ac6cceba3f5c2e481ecc86
Uni <a5304186@gmail.com>
2021-12-13 21:00:13 +0800

    test spilt.m

--- spilt.m
+++ spilt.m
@@ -1,37 +1,33 @@
-function [trainset, testset] = spilt(dataPath, spiltPercentage, random)
-    sprint("Dataset Spilting...");
-
+function spilt(dataPath, spiltPercentage, random)
     % read dataset
-    fileId = fopen(dataPath, "r");
-    % ratings = textscan(fileId, "%d%s%f", "Delimeter", ",", "HeaderLines", 1);
-    [userId, spots, rate] = textscan(fileId, "%d%s%f", "Delimeter", ",", "HeaderLines", 1);
-    fclose(fileId);
+    ratings = readmatrix(dataPath, "OutputType", "string");
+    % [userId, spots, rate] = textscan(fileId, "%d%s%f", "Delimeter", ",", "HeaderLines", 1);
 
     % spilt dataset to training set & testing set
-    trainset = MapNested();
-    testset = MapNested();
-    userIds = unique(userId);
-    start = 1; % 起始索引值
+    % trainset = MapNested();
+    % testset = MapNested();
+    % userIds = unique(userId);
+    % start = 1; % 起始索引值
 
-    for id = userIds
-        last = find(userId == id, 1, "last");
-        pos = round((last - start + 1) * spiltPercentage); % 資料分割點
-        currentIndex = start; % 目前索引值
+    % for id = userIds
+    %     last = find(userId == id, 1, "last");
+    %     pos = round((last - start + 1) * spiltPercentage); % 資料分割點
+    %     currentIndex = start; % 目前索引值
 
-        for spot = spots(start:last)
+    %     for spot = spots(start:last)
 
-            if currentIndex <= pos
-                trainset(user, spot) = rate(currentIndex);
-            else
-                testset(user, spot) = rate(currentIndex);
-            end
+    %         if currentIndex <= pos
+    %             trainset(user, spot) = rate(currentIndex);
+    %         else
+    %             testset(user, spot) = rate(currentIndex);
+    %         end
 
-            start = last + 1;
+    %         start = last + 1;
 
-        end
+    %     end
 
-    end
+    % end
 
-    sprint("Dataset Spilted!")
+    % sprint("Dataset Spilted!")
 
 end
