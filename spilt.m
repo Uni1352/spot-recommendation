27648a9951df59e2c75798b73c460fb77d723bf3
Uni <a5304186@gmail.com>
2021-12-13 22:59:41 +0800

    modified: spilt.m

--- spilt.m
+++ spilt.m
@@ -1,33 +1,29 @@
-function spilt(dataPath, spiltPercentage, random)
+function [trainset, testset] = spilt(dataPath, spiltPercentage, random)
     % read dataset
     ratings = readmatrix(dataPath, "OutputType", "string");
-    % [userId, spots, rate] = textscan(fileId, "%d%s%f", "Delimeter", ",", "HeaderLines", 1);
 
     % spilt dataset to training set & testing set
-    % trainset = MapNested();
-    % testset = MapNested();
-    % userIds = unique(userId);
-    % start = 1; % 起始索引值
+    userIds = str2double(ratings(:, 1)) % 所有使用者
+    trainsetIndex = []
+    start = 1; % 起始索引值
 
-    % for id = userIds
-    %     last = find(userId == id, 1, "last");
-    %     pos = round((last - start + 1) * spiltPercentage); % 資料分割點
-    %     currentIndex = start; % 目前索引值
+    for id = [1:length(unique(userIds))]
+        last = find(userIds == id, 1, "last")
+        index = [start:last]
+        pos = round((last - start + 1) * spiltPercentage) % 資料分割點
 
-    %     for spot = spots(start:last)
+        if random ~= 0
+            index = index(randperm(length(index)))
+        end
 
-    %         if currentIndex <= pos
-    %             trainset(user, spot) = rate(currentIndex);
-    %         else
-    %             testset(user, spot) = rate(currentIndex);
-    %         end
+        trainsetIndex = [trainsetIndex, sort(index(1:pos))]
+        start = last + 1
+    end
 
-    %         start = last + 1;
-
-    %     end
-
-    % end
-
-    % sprint("Dataset Spilted!")
-
+    writematrix(trainsetIndex, 'trainset.csv')
+    trainset = ratings(trainsetIndex, :)
+    ratings(trainsetIndex, :) = []
+    testset = ratings
+    writematrix(trainset, 'train.csv')
+    writematrix(testset, 'test.csv')
 end
